6e0a26d7d0ae8f306216c393017931ba5c80af50
Gabe Serna <gabriel.serna118@gmail.com>
2024-10-22 19:06:09 -0400

    Refactor Wavesurfer Player to be an HTML Element

--- app/audio/preview/page.tsx
+++ app/audio/preview/page.tsx
@@ -1,64 +1,43 @@
 "use client";
 
 import PianoRoll from "@/components/PianoRoll";
-import { useWavesurfer } from "@wavesurfer/react";
-import { useEffect, useRef, useState } from "react";
-import MinimapPlugin from "wavesurfer.js/dist/plugins/minimap";
+import WavesurferPlayer from "@wavesurfer/react";
+import { useCallback, useState } from "react";
+import WaveSurfer from "wavesurfer.js";
+// import RegionsPlugin from "wavesurfer.js/dist/plugins/regions";
 
 export default function Preview() {
-  const containerRef = useRef<HTMLDivElement | null>(null);
+  const [wavesurfer, setWavesurfer] = useState<WaveSurfer | null>(null);
   const [isPlaying, setIsPlaying] = useState(false);
   const [midiFile, setMidiFile] = useState<File | null>(null);
 
-  const { wavesurfer, isReady, currentTime } = useWavesurfer({
-    container: containerRef,
-    url: "/audio/tsubasa_bass.mp3",
-    waveColor: "#eab308",
-    progressColor: "#a16207",
-    height: 200,
-    barWidth: 5,
-    barHeight: 4,
-    barRadius: 5,
-    autoScroll: true,
-    autoCenter: true,
-    minPxPerSec: 100,
-    // plugins: [
-    //   // Register the plugin
-    //   MinimapPlugin.create({
-    //     height: 20,
-    //     waveColor: "#ddd",
-    //     progressColor: "#999",
-    //     // the Minimap takes all the same options as the WaveSurfer itself
-    //   }),
-    // ],
-  });
+  const onReady = useCallback((wavesurfer: WaveSurfer) => {
+    setWavesurfer(wavesurfer);
+    setIsPlaying(false);
+  }, []);
 
-  useEffect(() => {
-    if (wavesurfer && isReady) {
-      wavesurfer.registerPlugin(
-        MinimapPlugin.create({
-          height: 50,
-          waveColor: "#666",
-          progressColor: "#444",
-          barWidth: 4,
-          barHeight: 5,
-          barRadius: 5,
-        }),
-      );
-    }
-  }, [isReady, wavesurfer]);
-
-  useEffect(() => {
+  const onPlayPause = useCallback(() => {
     wavesurfer && wavesurfer.playPause();
-  }, [isPlaying]);
+  }, [wavesurfer]);
 
   return (
     <div className="flex h-full w-[1000px] flex-col justify-center">
-      <div ref={containerRef} />
+      <WavesurferPlayer
+        url="/audio/tsubasa_bass.mp3"
+        waveColor="#eab308"
+        progressColor="#a16207"
+        height={200}
+        barWidth={5}
+        barHeight={4}
+        barRadius={5}
+        autoScroll={true}
+        autoCenter={true}
+        onReady={onReady}
+        onPlay={() => setIsPlaying(true)}
+        onPause={() => setIsPlaying(false)}
+      />
 
-      <button onClick={() => setIsPlaying(!isPlaying)}>
-        {isPlaying ? "Pause" : "Play"}
-      </button>
+      <button onClick={onPlayPause}>{isPlaying ? "Pause" : "Play"}</button>
       <div>
         <input
           type="file"
