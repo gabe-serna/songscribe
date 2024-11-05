50f837b2c862e89647626857bc01781d3d336793
Gabe Serna <gabriel.serna118@gmail.com>
2024-11-05 17:06:50 -0500

    Add React Transition Group

--- app/audio/AudioForm.tsx
+++ app/audio/AudioForm.tsx
@@ -1,24 +1,43 @@
 "use client";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
+import { CSSTransition } from "react-transition-group";
 import AudioPart1 from "@/app/audio/AudioPart1";
 import { AudioContext } from "@/app/audio/AudioProvider";
 
 export default function AudioForm() {
-  const { audioForm, setAudioForm } = useContext(AudioContext);
+  const { audioForm } = useContext(AudioContext);
   const isPart1Complete = audioForm.audio_file || audioForm.audio_link;
-  const isPart2Complete = audioForm.separation_mode;
+  const [isPart2Visible, setIsPart2Visible] = useState(false);
+
+  useEffect(() => {
+    if (audioForm.separation_mode) setIsPart2Visible(false);
+  }, [audioForm]);
 
   return (
-    <div className="flex h-[50vh] flex-col items-center justify-center space-y-16">
-      {!isPart1Complete && (
-        <>
+    <div className="flex h-[50vh]">
+      <CSSTransition
+        in={!isPart1Complete}
+        timeout={700}
+        classNames="fade"
+        unmountOnExit
+        onExited={() => setIsPart2Visible(true)}
+      >
+        <div className="flex flex-col items-center justify-center space-y-16">
           <h1 className="w-[15ch] text-center text-3xl font-bold lg:w-auto lg:text-4xl">
             The fastest way to turn any song into sheet music
           </h1>
           <AudioPart1 />
-        </>
-      )}
-      {isPart1Complete && !isPart2Complete && <h2>Part 2</h2>}
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={isPart2Visible}
+        timeout={700}
+        classNames="fade"
+        unmountOnExit
+      >
+        <h2>Part 2</h2>
+      </CSSTransition>
     </div>
   );
 }
