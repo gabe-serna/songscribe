f731c226cf31179ef3cfcc9950d5c8afb3299d70
Gabe Serna <gabriel.serna118@gmail.com>
2024-11-11 16:48:10 -0500

    Adjust Homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+"use client";
+import { Button } from "@/components/ui/button";
+
 export default async function Index() {
   return (
     <>
-      <main className="flex flex-1 flex-col gap-6 px-4">
-        <h1 className="mb-4 text-xl font-semibold">Songscribe</h1>
-        <p>The fastest way to turn any song into sheet music</p>
+      <main className="flex flex-1 flex-col justify-center px-4">
+        <h1 className="text-xl font-semibold">Songscribe</h1>
+        <p className="mt-2">
+          The fastest way to turn any song into sheet music
+        </p>
+        <Button
+          onClick={() => (window.location.href = "/audio")}
+          className="button-primary mt-8"
+        >
+          Get Started
+        </Button>
       </main>
     </>
   );
